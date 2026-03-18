ba5b9fd7e41d01d246be8e229627c4951a11f4d6
yushan777 <yushanyu2001@gmail.com>
2026-03-18 09:46:56 +0000

    Update aspect_ratio_picker.js

--- web/js/aspect_ratio_picker.js
+++ web/js/aspect_ratio_picker.js
@@ -10,9 +10,11 @@ class Y7AspectRatioPicker
         this.node.properties = { valueX:1024, valueY:1024, minX:0, minY:0, maxX:2048, maxY:2048, stepX:64, stepY:64, snap: true, dots: true };
         this.node.helpIconOffsetX = 14;
         this.node.intpos = { x:0.5, y:0.5 };
-        const min_width_height = 200;
+        const min_width_height = 160;
+        const shiftLeftTopBottom = 10;  // padding inset on the left, top, and bottom edges of the drawing canvas
+        const shiftRight = 60; // reserved width on the right side for the output values/labels panel
 
-        this.node.size = [min_width_height, min_width_height];
+        this.node.size = [min_width_height + shiftRight - shiftLeftTopBottom, min_width_height];
         const fontsize = LiteGraph.NODE_SUBTEXT_SIZE;
         const shX = (this.node.slot_start_y || 0)+fontsize*1.5;
         const shY = shX + LiteGraph.NODE_SLOT_HEIGHT;
@@ -23,8 +25,6 @@ class Y7AspectRatioPicker
             const d = gcd(x, y);
             return `${x/d}:${y/d}`;
         }
-        const shiftLeft = 10;
-        const shiftRight = 60;
 
         for (let i=0; i<2; i++) { this.node.widgets[i].hidden = true; this.node.widgets[i].type = "hidden"; }
 
@@ -35,7 +35,7 @@ class Y7AspectRatioPicker
             this.widgets_start_y = -4.8e8*LiteGraph.NODE_SLOT_HEIGHT;
             this.intpos.x = (this.properties.valueX - this.properties.minX) / (this.properties.maxX - this.properties.minX);
             this.intpos.y = (this.properties.valueY - this.properties.minY) / (this.properties.maxY - this.properties.minY);
-            if (this.size[1] > this.size[0]-shiftRight+shiftLeft) {this.size[1] = this.size[0]-shiftRight+shiftLeft} else {this.size[0] = this.size[1]+shiftRight-shiftLeft}
+            if (this.size[1] > this.size[0]-shiftRight+shiftLeftTopBottom) {this.size[1] = this.size[0]-shiftRight+shiftLeftTopBottom} else {this.size[0] = this.size[1]+shiftRight-shiftLeftTopBottom}
         };
 
         this.node.onConfigure = function () {}
@@ -55,10 +55,10 @@ class Y7AspectRatioPicker
             if ( isNaN(this.properties.valueY) ) this.properties.valueY = this.properties.minY;
             if ( this.properties.minX >= this.properties.maxX ) this.properties.maxX = this.properties.minX+1;
             if ( this.properties.minY >= this.properties.maxY ) this.properties.maxY = this.properties.minY+1;
-            if ((propName === "minX") && ( this.properties.valueX < this.properties.minX )) this.properties.valueX = this.properties.minX;
-            if ((propName === "minY") && ( this.properties.valueY < this.properties.minY )) this.properties.valueY = this.properties.minY;
-            if ((propName === "maxX") && ( this.properties.valueX > this.properties.maxX )) this.properties.valueX = this.properties.maxX;
-            if ((propName === "maxY") && ( this.properties.valueY > this.properties.maxY )) this.properties.valueY = this.properties.maxY;
+            if ((propName === "minX" || propName === "valueX") && ( this.properties.valueX < this.properties.minX )) this.properties.valueX = this.properties.minX;
+            if ((propName === "minY" || propName === "valueY") && ( this.properties.valueY < this.properties.minY )) this.properties.valueY = this.properties.minY;
+            if ((propName === "maxX" || propName === "valueX") && ( this.properties.valueX > this.properties.maxX )) this.properties.valueX = this.properties.maxX;
+            if ((propName === "maxY" || propName === "valueY") && ( this.properties.valueY > this.properties.maxY )) this.properties.valueY = this.properties.maxY;
             this.properties.valueX = Math.round(this.properties.valueX);
             this.properties.valueY = Math.round(this.properties.valueY);
             this.intpos.x = Math.max(0, Math.min(1, (this.properties.valueX-this.properties.minX)/(this.properties.maxX-this.properties.minX)));
@@ -75,7 +75,7 @@ class Y7AspectRatioPicker
 
             ctx.fillStyle="rgba(20,20,20,0.8)";
             ctx.beginPath();
-            ctx.roundRect( shiftLeft-4, shiftLeft-4, this.size[0]-shiftRight-shiftLeft+8, this.size[1]-shiftLeft-shiftLeft+8,4);
+            ctx.roundRect( shiftLeftTopBottom-4, shiftLeftTopBottom-4, this.size[0]-shiftRight-shiftLeftTopBottom+8, this.size[1]-shiftLeftTopBottom-shiftLeftTopBottom+8,4);
             ctx.fill();
 
             // Dots
@@ -83,30 +83,30 @@ class Y7AspectRatioPicker
             {
                 ctx.fillStyle="rgba(200,200,200,0.7)";
                 ctx.beginPath();
-                let swX = (this.size[0]-shiftRight-shiftLeft);
-                let swY = (this.size[1]-shiftLeft-shiftLeft);
+                let swX = (this.size[0]-shiftRight-shiftLeftTopBottom);
+                let swY = (this.size[1]-shiftLeftTopBottom-shiftLeftTopBottom);
                 let stX = (swX*this.properties.stepX/(this.properties.maxX-this.properties.minX));
                 let stY = (swY*this.properties.stepY/(this.properties.maxY-this.properties.minY));
-                for (var ix=0; ix<swX+stX/2; ix+=stX) for (var iy=0; iy<swY+stY/2; iy+=stY) ctx.rect(shiftLeft+ix-0.5, shiftLeft+iy-0.5, 1, 1);
+                for (var ix=0; ix<swX+stX/2; ix+=stX) for (var iy=0; iy<swY+stY/2; iy+=stY) ctx.rect(shiftLeftTopBottom+ix-0.5, shiftLeftTopBottom+iy-0.5, 1, 1);
                 ctx.fill();
             }
 
             ctx.fillStyle="rgba(183, 144, 255, 0.2)";
             ctx.strokeStyle="rgba(228, 17, 17, 0.7)";
             ctx.beginPath();
-            ctx.rect(shiftLeft, shiftLeft+(this.size[1]-shiftLeft-shiftLeft)*(1-this.intpos.y),(this.size[0]-shiftRight-shiftLeft)*this.intpos.x,(this.size[1]-shiftLeft-shiftLeft)*(this.intpos.y));
+            ctx.rect(shiftLeftTopBottom, shiftLeftTopBottom+(this.size[1]-shiftLeftTopBottom-shiftLeftTopBottom)*(1-this.intpos.y),(this.size[0]-shiftRight-shiftLeftTopBottom)*this.intpos.x,(this.size[1]-shiftLeftTopBottom-shiftLeftTopBottom)*(this.intpos.y));
             ctx.fill();
             ctx.stroke();
 
             ctx.fillStyle=LiteGraph.NODE_TEXT_COLOR;
             ctx.beginPath();
-            ctx.arc(shiftLeft+(this.size[0]-shiftRight-shiftLeft)*this.intpos.x, shiftLeft+(this.size[1]-shiftLeft-shiftLeft)*(1-this.intpos.y), 7, 0, 2 * Math.PI, false);
+            ctx.arc(shiftLeftTopBottom+(this.size[0]-shiftRight-shiftLeftTopBottom)*this.intpos.x, shiftLeftTopBottom+(this.size[1]-shiftLeftTopBottom-shiftLeftTopBottom)*(1-this.intpos.y), 5, 0, 2 * Math.PI, false);
             ctx.fill();
 
             ctx.lineWidth = 1.5;
-            ctx.strokeStyle=node.bgcolor || LiteGraph.NODE_DEFAULT_BGCOLOR;
+            ctx.strokeStyle=this.bgcolor || LiteGraph.NODE_DEFAULT_BGCOLOR;
             ctx.beginPath();
-            ctx.arc(shiftLeft+(this.size[0]-shiftRight-shiftLeft)*this.intpos.x, shiftLeft+(this.size[1]-shiftLeft-shiftLeft)*(1-this.intpos.y), 5, 0, 2 * Math.PI, false);
+            ctx.arc(shiftLeftTopBottom+(this.size[0]-shiftRight-shiftLeftTopBottom)*this.intpos.x, shiftLeftTopBottom+(this.size[1]-shiftLeftTopBottom-shiftLeftTopBottom)*(1-this.intpos.y), 3, 0, 2 * Math.PI, false);
             ctx.stroke();
 
             ctx.font = (fontsize) + "px Arial";
@@ -149,7 +149,6 @@ class Y7AspectRatioPicker
         {
             if (e.canvasY - this.pos[1] < 0) return _origMouseDown ? _origMouseDown.apply(this, arguments) : false;
             if (e.shiftKey &&
-                e.canvasX > this.pos[0]+this.size[0]-shiftRight+10 &&
                 e.canvasX < this.pos[0]+this.size[0]-15 &&
                 e.canvasX > this.pos[0]+this.size[0]-shiftRight+15 &&
                 e.canvasY < this.pos[1]+shY &&
@@ -170,8 +169,8 @@ class Y7AspectRatioPicker
                 return true;
             }
 
-if ( e.canvasX < this.pos[0]+shiftLeft-5 || e.canvasX > this.pos[0]+this.size[0]-shiftRight+5 ) return false;
-            if ( e.canvasY < this.pos[1]+shiftLeft-5 || e.canvasY > this.pos[1]+this.size[1]-shiftLeft+5 )  return false;
+if ( e.canvasX < this.pos[0]+shiftLeftTopBottom-5 || e.canvasX > this.pos[0]+this.size[0]-shiftRight+5 ) return false;
+            if ( e.canvasY < this.pos[1]+shiftLeftTopBottom-5 || e.canvasY > this.pos[1]+this.size[1]-shiftLeftTopBottom+5 )  return false;
             this.capture = true;
             this.captureInput(true);
             this.valueUpdate(e);
@@ -189,8 +188,8 @@ if ( e.canvasX < this.pos[0]+shiftLeft-5 || e.canvasX > this.pos[0]+this.size[0]
         {
             let prevX = this.properties.valueX;
             let prevY = this.properties.valueY;
-            let vX = (e.canvasX - this.pos[0] - shiftLeft)/(this.size[0]-shiftRight-shiftLeft);
-            let vY = 1-(e.canvasY - this.pos[1] - shiftLeft)/(this.size[1]-shiftLeft-shiftLeft);
+            let vX = (e.canvasX - this.pos[0] - shiftLeftTopBottom)/(this.size[0]-shiftRight-shiftLeftTopBottom);
+            let vY = 1-(e.canvasY - this.pos[1] - shiftLeftTopBottom)/(this.size[1]-shiftLeftTopBottom-shiftLeftTopBottom);
             if (e.shiftKey !== this.properties.snap)
             {
                 let sX = this.properties.stepX/(this.properties.maxX - this.properties.minX);
@@ -221,9 +220,9 @@ if ( e.canvasX < this.pos[0]+shiftLeft-5 || e.canvasX > this.pos[0]+this.size[0]
         this.node.resizable = true;
         this.node.onResize = function(size) {
             if (size[1] < min_width_height) size[1] = min_width_height;
-            size[0] = size[1] + shiftRight - shiftLeft;
+            size[0] = size[1] + shiftRight - shiftLeftTopBottom;
         };
-        this.node.computeSize = () => [min_width_height + shiftRight - shiftLeft, min_width_height];
+        this.node.computeSize = () => [min_width_height + shiftRight - shiftLeftTopBottom, min_width_height];
     }
 }
 
